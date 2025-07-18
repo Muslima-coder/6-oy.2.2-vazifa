c697c29aef376230e0b5dffcc2cb89b4d4d4b6a9
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-18 23:42:59 +0500

    update commit

--- src/modules/listTodo.jsx
+++ src/modules/listTodo.jsx
@@ -16,15 +16,15 @@ const listTodo = ({todos, setTodos}) => {
     <div>
       <ul className="flex justify-center mx-auto sm:justify-between p-20 items-center flex-wrap  w-full gap-[20px]">
         {todos.map((item, index) => (
-          <li key={item.id} className="w-[300px]  p-3 rounded-md shadow-xl shadow-slate-400 flex justify-between ">
+          <li key={item.id} className="sm:w-[300px] w-[250px] p-3 rounded-md shadow-xl shadow-slate-400 flex justify-between ">
             <div className="flex items-center gap-1">
-              <span className="text-[17px] font-medium text-white">#{index +1}</span>
-              <strong className="text-[17px] font-medium text-white">{item.title}</strong>
+              <span className="sm:text-[17px] text-[14px] font-medium text-white">#{index +1}</span>
+              <strong className="sm:text-[17px] text-[14px] font-medium text-white">{item.title}</strong>
             </div>
 
             <div className="flex items-center gap-2 ">
-              <button onClick={() =>handleEdit(item.id)} className="border-[#3A70E2] border-[2px] cursor-pointer text-[15px] font-medium text-[#3A70E2] py-2 px-4 rounded-md hover:scale-[1.04] ">Edit</button>
-              <button onClick={() => handleDelete(index)} className="border-[#ff4d4d] border-[2px] cursor-pointer text-[15px] font-medium text-[#ff4d4d] py-2 px-4 rounded-md hover:scale-[1.04] ">Delete</button>
+              <button onClick={() =>handleEdit(item.id)} className="border-[#3A70E2] border-[2px] cursor-pointer text-[13px] sm:text-[15px] font-medium text-[#3A70E2] sm:py-2 sm:px-4  py-1 px-2 rounded-md hover:scale-[1.04] ">Edit</button>
+              <button onClick={() => handleDelete(index)} className="border-[#ff4d4d] border-[2px] cursor-pointer text-[13px] sm:text-[15px] font-medium text-[#ff4d4d] sm:py-2 sm:px-4  py-1 px-2 rounded-md hover:scale-[1.04] ">Delete</button>
             </div>
           </li>
         ))}
