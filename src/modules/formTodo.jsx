067726d2b9c193ba096c60fc283ebdc5825d59ee
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-18 23:45:56 +0500

    update commit

--- src/modules/formTodo.jsx
+++ src/modules/formTodo.jsx
@@ -15,7 +15,7 @@ const formTodo = ({todos, setTodos}) => {
 return(
     <form onSubmit={handleSubmit} className="bg-transparent border-[2px] border-slate-300 p-4 rounded-xl w-[300px] sm:w-[380px] mx-auto mt-5 flex justify-between items-center">
         <input maxLength={10} className="outline-none w-[78%] border-[1.5px] text-white border-purple-900 shadow-md   p-2 rounded-md" autoComplete="off" required type="text" placeholder="Enter your todo" name="todo" />
-        <button className="w-[20%] py-2 rounded-md cursor-pointer border-[1.5px] border-purple-900 text-white hover:scale-[1.04] duration-500" type="submit">Create</button>
+        <button className="w-[20%] sm:text-[18px] text-[13px] py-2 rounded-md cursor-pointer border-[1.5px] border-purple-900 text-white hover:scale-[1.04] duration-500" type="submit">Create</button>
       </form>
 )
 }
